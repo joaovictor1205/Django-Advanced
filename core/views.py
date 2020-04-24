b966f90a703800f7832fac397b16ac4b22442095
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-04-24 19:27:03 +0000

    starting project :computer:

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context = {
+            'teste': 'teste',
+        }
+
+        return context
