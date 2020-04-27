0fc64a8ac06a76ed3ee95f3ca4707490692fd459
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-04-27 20:48:06 +0000

    Finalizando Detail View

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from django.views.generic.base import TemplateView
-from .views import HomePageView, View, PersonList
+from .views import HomePageView, View, PersonList, PersonDetail
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(template_name="home.html"), name="home"),
-    path('view/', View.as_view(), name="view"),
-    path('list/', PersonList.as_view(), name="list"),
+    path('inicio/', HomePageView.as_view(template_name="home.html"), name="inicio"),
+    path('visualizar/', View.as_view(), name="visualizar"),
+    path('lista/', PersonList.as_view(), name="lista"),
+    path('detalhe/<int:pk>', PersonDetail.as_view(), name="detalhe")
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Person
+from django.utils import timezone
+
+from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from django.http import HttpResponse
-from django.views import View
-from .models import Person
+from django.views.generic.detail import DetailView
 
 class HomePageView(TemplateView):
 
@@ -28,3 +31,11 @@ class View(View):
 
 class PersonList(ListView):
     model = Person
+
+class PersonDetail(DetailView):
+    model = Person
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = timezone.now()
+        return context
